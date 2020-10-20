c2323cba581def1d3452712d077661f320eda063
mrbjm1994 <mrbjm1994@gmail.com>
2020-10-20 14:31:52 +0100

    completed

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+$(document).on('show', '.accordion', function(e) {
+    //$('.accordion-heading i').toggleClass(' ');
+    $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+});
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+$(document).on('hide', '.accordion', function(e) {
+    $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+    //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
+});
